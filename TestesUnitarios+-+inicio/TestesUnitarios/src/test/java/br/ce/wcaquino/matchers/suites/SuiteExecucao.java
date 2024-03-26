97b9358ea7189835ff1f974052e918769d0dade0
Raul <raul.cfr112@gmail.com>
2024-03-26 09:58:31 -0400

    Adicionado suite de testes.

---
+++ TestesUnitarios+-+inicio/TestesUnitarios/src/test/java/br/ce/wcaquino/matchers/suites/SuiteExecucao.java
@@ -0,0 +1,20 @@
+package br.ce.wcaquino.matchers.suites;
+
+import br.ce.wcaquino.servicos.CalculadoraTest;
+import br.ce.wcaquino.servicos.CalculoValorLocacaoTest;
+import br.ce.wcaquino.servicos.LocacaoServiceTest;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CalculadoraTest.class,
+        CalculoValorLocacaoTest.class,
+        LocacaoServiceTest.class
+})
+public class SuiteExecucao {
+    //Remova se puder!
+
+}
